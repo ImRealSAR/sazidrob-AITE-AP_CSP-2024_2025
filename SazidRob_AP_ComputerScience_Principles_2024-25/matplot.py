7485467e47fde8246622f887aa9a9c7602c2d0d7
SAR <96535189+ImRealSAR@users.noreply.github.com>
2025-03-10 16:10:17 -0400

    rtyu

--- SazidRob_AP_ComputerScience_Principles_2024-25/matplot.py
+++ SazidRob_AP_ComputerScience_Principles_2024-25/matplot.py
@@ -2,67 +2,61 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tkinter import *
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
-NavigationToolbar2Tk)
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
-
-root=Tk()
+root = Tk()
 root.title('School Data')
 root.geometry('800x800')
 
-
 e = 0
-entries=[]
-data_entries=[]
-def add_entry():
-   global e, entries, data_entries
-   school_entry=Entry(root,width=20)
-   school_entry.grid(row=e,column=1,sticky=W)   
-   entries.append(school_entry)
-   e += 1
-   for i in range(len(entries)):
-       school_label=Label(root,text="Enter Data Figure # " + str(i+1),width=15)
-       school_label.grid(row=i,column=0,sticky=W)
-   data_label=Entry(root,width=20)
-   data_label.grid(row=e-1,column=3,sticky=W)
-   data_entries.append(data_label)
-   for i in range(len(data_entries)):
-       school_label=Label(root,text="Enter Data Label " + str(i+1),width=15)
-       school_label.grid(row=i,column=2,sticky=W)
+entries = []
+data_entries = []
 
+def add_entry():
+    global e, entries, data_entries
+    school_entry = Entry(root, width=20)
+    school_entry.grid(row=e, column=1, sticky=W)
+    entries.append(school_entry)
+    e += 1
+    for i in range(len(entries)):
+        school_label = Label(root, text="Enter Data Figure # " + str(i+1), width=15)
+        school_label.grid(row=i, column=0, sticky=W)
+    data_label = Entry(root, width=20)
+    data_label.grid(row=e-1, column=3, sticky=W)
+    data_entries.append(data_label)
+    for i in range(len(data_entries)):
+        data_label = Label(root, text="Enter Data Label " + str(i+1), width=15)
+        data_label.grid(row=i, column=2, sticky=W)
 
 def delete_row():
-   global entries, data_entries
-   entries.pop()
-   data_entries.pop()
+    global entries, data_entries
+    if entries:
+        entries.pop().destroy()
+    if data_entries:
+        data_entries.pop().destroy()
 
-
-canvas = Canvas(root,width=400,height=400)
 def create_graph():
-   global entries, canvas
-   entry_data = []
-   data_labels = []
-   fig=Figure(figsize= (5,5),dpi=100)
-   l = len(entries) + 1
-   for i in range(len(entries)):
-       upd_entries = entries[i].get()
-       entry_data.append(int(upd_entries))
-   for i in range(len(data_entries)):
-       upd_data_entries = data_entries[i].get
-       data_labels.append(upd_data_entries)
-  
-   gen_data=np.array(entry_data)
+    global entries, canvas
+    entry_data = []
+    data_labels = []
+    fig = Figure(figsize=(5, 5), dpi=100)
+    for entry in entries:
+        entry_data.append(int(entry.get()))
+    for data_entry in data_entries:
+        data_labels.append(data_entry.get())
+    
+    gen_data = np.array(entry_data)
+    plot1 = fig.add_subplot(111)
+    plot1.scatter(data_labels, gen_data)
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.draw()
+    canvas.get_tk_widget().grid(row=e+1, column=0, sticky=E)
 
-
-   plot1=fig.add_subplot(111)
-   plot1.scatter(data_labels,gen_data)
-   canvas = FigureCanvasTkAgg(fig,master=root)
-   canvas.draw()
-   canvas.get_tk_widget().grid(row=e+1,column=0,sticky=E)
-
-
-graph_button = Button(root, tex="Create Graph",width=15,command=create_graph).grid(row=e+1,column=4)
-add_row_button = Button(root,text="Add Row",width=15,command=add_entry).grid(row=e+2,column=4)
-delete_row = Button(root,text="Delete Row",width=15,command=delete_row).grid(row=e+3,column=4)
+graph_button = Button(root, text="Create Graph", width=15, command=create_graph)
+graph_button.grid(row=e+1, column=4)
+add_row_button = Button(root, text="Add Row", width=15, command=add_entry)
+add_row_button.grid(row=e+2, column=4)
+delete_row_button = Button(root, text="Delete Row", width=15, command=delete_row)
+delete_row_button.grid(row=e+3, column=4)
 
 root.mainloop()
