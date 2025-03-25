dea4bc2c50975b93f1ec028a50b2d79f8345541f
SAR <96535189+ImRealSAR@users.noreply.github.com>
2025-03-24 20:43:08 -0400

    Update main.py

--- SazidRob_AP_ComputerScience_Principles_2024-25/activity 321 sazid/main.py
+++ SazidRob_AP_ComputerScience_Principles_2024-25/activity 321 sazid/main.py
@@ -4,17 +4,44 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+my_countires = ["Palestine", "Brazil", "Argentina" , "Columbia", "Bangladessh", "Mexico"]
 # Load in the data with read_csv()
 # TODO #1: change the file name to your data file name
 temp_data = pd.read_csv("temperature_data.csv", header=0)   # identify the header row
 
-# TODO #2: Use matplotlib to make a line graph
-
-
 # TODO #3: Plot LOWESS in a line graph
+plt.plot(temp_data['Year'], temp_data['LOWESS'], color='blue')
+plt.ylabel('Temperature Anomalies in Celsius')
+plt.xlabel('Years')
+plt.title('Change in Temperatures')
+plt.show()
 
 
 # TODO #4: Use matplotlib to make a bar chart
-
+# TODO #4: Use matplotlib to make a bar chart
+plt.bar(temp_data['Year'], temp_data['Anomaly'], align='center', color='green')
+plt.ylabel('Temperature Anomalies in Celsius')
+plt.xlabel('Years')
+plt.title('Change in Temperatures')
+plt.show()
 
 # TODO #5: Calculate min, max, and avg anomaly and the corresponding min/max years
+# TODO #5: Calculate min, max, and avg anomaly and the corresponding min/max years
+min_anomaly = temp_data['Anomaly'][0]
+max_anomaly = temp_data['Anomaly'][0]
+min_year = temp_data['Year'][0]
+max_year = temp_data['Year'][0]
+sum_anomaly = 0
+avg_anomaly = 0
+
+# find the min, max and calculate the sum
+for i in range(len(temp_data['Anomaly'])):
+  if (temp_data['Anomaly'][i] > max_anomaly):
+    max_anomaly = temp_data['Anomaly'][i]
+    max_year = temp_data['Year'][i]
+    sum_anomaly += temp_data['Anomaly'][i]
+
+# print the statistical values
+print("The maximum anomaly is:", max_anomaly, "which occured in", max_year)
+print("The minimum anomaly is:", min_anomaly, "which occured in", min_year)
+print("The average anomaly is:", avg_anomaly)
